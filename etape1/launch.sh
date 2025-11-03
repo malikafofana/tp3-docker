302a6854551b3cee0c1f44b1b1efe1fa0f8c6412
malikaaivancity <malika.fofana@aivancity.education>
2025-11-03 09:18:05 +0100

    TP3 fini - Docker, SQL, Docker Compose

--- etape1/launch.sh
+++ etape1/launch.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Nettoyage idempotent
-docker rm -f script http 2>/dev/null || true
-
-# PHP-FPM (SCRIPT)
-docker run -d --name script \
-  --network tp3net \
-  -v "$(pwd)/src:/app" \
-  php:8.2-fpm
-
-# Nginx (HTTP)
-docker run -d --name http \
-  --network tp3net \
-  -p 8080:80 \
-  -v "$(pwd)/src:/app" \
-  -v "/tmp/default.conf:/etc/nginx/conf.d/default.conf" \
-  nginx:alpine
-
-
-
-
-
-
+#
